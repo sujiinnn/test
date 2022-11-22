b315b0973f2eb57d985b02204a139b289223a79a
sujin <sujiinnn@gmail.com>
2022-11-23 02:24:09 +0900

    enter

--- Hodadak/src/hodadak/Hodadak.java
+++ Hodadak/src/hodadak/Hodadak.java
@@ -14,7 +14,8 @@ public class Hodadak {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic hereS
+        
     }
     
 }
